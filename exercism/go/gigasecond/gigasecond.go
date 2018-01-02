0dc1e55cb54d4ecebdc792e6bde8a6ae1feae049
ratulotron <m@ratul.xyz>
2018-01-02 17:51:39 +0600

    Trying out but I guess I need to go back to the basics

--- exercism/go/gigasecond/gigasecond.go
+++ exercism/go/gigasecond/gigasecond.go
@@ -3,10 +3,12 @@
 
 // Package gigasecond should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
-package gigasecond
+// package gigasecond
+package main
 
 // import path for the time package from the standard library
 import "time"
+import "fmt"
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
@@ -14,5 +16,19 @@ func AddGigasecond(t time.Time) time.Time {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
+
+	gigasecond := 1000000000
+	// fmt.Println(gigasecond)
+
+	now := time.Now()
+	difference := now.Sub(t)
+
+	fmt.Println(t)
+
 	return t
+}
+
+func main() {
+	var t time.Time = time.Now()
+	AddGigasecond(t)
 }
