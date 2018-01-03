ee1801a4470079df33dc2d0c34df839ff884f81d
ratulotron <m@ratul.xyz>
2018-01-03 18:07:41 +0600

    Trying out but I guess I need to go back to the basics

--- exercism/go/gigasecond/gigasecond.go
+++ exercism/go/gigasecond/gigasecond.go
@@ -1,34 +1,16 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+/*
+Package gigasecond has a function AddGigasecond that takes someone's birthday
+and calculates when someone passes a gigasecond (1000000000) of their lives.
+*/
+package gigasecond
 
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
-// package gigasecond
-package main
-
-// import path for the time package from the standard library
 import "time"
-import "fmt"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond takes someone's birthday and returns when they pass a
+// gigasecond age.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-
-	gigasecond := 1000000000
-	// fmt.Println(gigasecond)
-
-	now := time.Now()
-	difference := now.Sub(t)
-
-	fmt.Println(t)
-
-	return t
-}
-
-func main() {
-	var t time.Time = time.Now()
-	AddGigasecond(t)
+	// const GigaInt = 1000000000
+	// var gigaseconds = time.Duration(GigaInt) * time.Second
+	return t.Add(1000000000)
+	// return t.Add(gigaseconds)
 }
