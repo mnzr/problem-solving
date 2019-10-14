0aa2e6c14305b03aa21a10d04f6770c3218b10f2
Ratul Minhaz <minhaz.ratul@gmail.com>
2019-10-14 09:02:11 +0600

    Bob

--- exercism/go/bob/bob.go
+++ exercism/go/bob/bob.go
@@ -3,41 +3,70 @@ package bob
 // package main
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
+
+func isNumOnly(remark string) bool {
+	reg, _ := regexp.Compile("[^0-9]+")
+	processedString := reg.ReplaceAllString(remark, "")
+	if processedString == "" {
+		return false
+	}
+	return true
+}
+
+func isQuestion(remark string) bool {
+	if string(remark[len(remark)-1]) == "?" {
+		return true
+	}
+	return false
+}
+
+func isExclamation(remark string) bool {
+	if string(remark[len(remark)-1]) == "!" {
+		return true
+	}
+	return false
+}
+
+func isYelling(remark string) bool {
+	if !isNumOnly(remark) && remark == strings.ToUpper(remark) {
+		return true
+	}
+	return false
+}
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
 	remark = strings.TrimSpace(remark)
 
+	// if you address him without actually saying anything.
 	if remark == "" {
 		return "Fine. Be that way!"
 	}
 
-	length := len(remark)
-	last_char := remark[length-1]
-	// fmt.Println(remark[length-1])
-
-	if last_char == '?' {
-		if remark == strings.ToUpper(remark) {
+	if isQuestion(remark) {
+		if isYelling(remark) {
 			return "Calm down, I know what I'm doing!"
 		}
 		return "Sure."
 	}
-	if remark == strings.ToUpper(remark) {
+
+	if isYelling(remark) {
 		return "Whoa, chill out!"
 	}
 
-	if remark == "Bob" {
-		return "Whoa, chill out!"
+	if isNumOnly(remark) {
+		return "Sure."
 	}
-
 	return "Whatever."
 }
 
-// func main() {
-// 	// fmt.Println(Hey("Ki hoise?"))
-// 	// fmt.Println(Hey("KI HOISE!"))
-// 	// fmt.Println(Hey("Ki hoise!"))
-// 	// fmt.Println(Hey("\t\t\t\t\t\t\t\t\t\t"))
-// }
+func main() {
+	fmt.Println(Hey("Ki hoise?"))
+	fmt.Println(Hey("KI HOISE!"))
+	fmt.Println(Hey("Ki hoise!"))
+	fmt.Println(Hey("\t\t\t\t\t\t\t\t\t\t"))
+}
